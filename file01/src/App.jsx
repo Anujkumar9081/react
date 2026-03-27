d070e1c7e3c6e048471c805c315ae756606f069b
anuj kumar <anujyadav9081Anu@gmail.com>
2026-03-27 13:03:31 +0530

    learing git from youtube

---
+++ file01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>anuj yadav</h1>
+      <h1>Vedam school of technology </h1>
+    </div>
+  )
+}
+
+export default App
