fcbf1f420de6e5d7bd1e40784d03ae33eab52dc5
anuj kumar <anujyadav9081Anu@gmail.com>
2026-04-08 18:23:46 +0530

    done

---
+++ project 2/src/components/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className="bg-amber-300 h-screen font-bold"> 
+        this is section 1
+    </div>
+  )
+}
+
+export default Section1
---
+++ project 2/src/components/Section2/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className="bg-green-300 h-screen font-bold">
+      this is section 2
+    </div>
+  )
+}
+
+export default Section2
