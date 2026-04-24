94ff8ea9d88f1c84a32174bfd64f29d8e5d054dd
anuj kumar <anujyadav9081Anu@gmail.com>
2026-04-25 02:55:56 +0530

    done

---
+++ file02/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>anuj yadav</h1>
+      <h1>vedam school of techonology</h1>
+    </div>
+  )
+}
+
+export default App
